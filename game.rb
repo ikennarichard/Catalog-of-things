36a24865e1a08328256fa0f7d0d0a3027dc79c2d
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-04 20:01:51 +0500

    Add Game class inheriting from Item

---
+++ game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date: nil, archived: false)
+    super(publish_date, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    years_since_last_played = Time.now.year - Date.parse(last_played_at).year
+    super && years_since_last_played > 2
+  end
+end
