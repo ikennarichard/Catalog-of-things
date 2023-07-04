7ae3422fa02be754e1edad328c49545d996f11c6
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-05 02:51:11 +0500

    Fix linter errors

--- item.rb
+++ item.rb
@@ -1,18 +1,21 @@
 require 'date'
 class Item
-  
-attr_reader :id
-attr_accessor :publish_date, :archived
+  attr_reader :id
+  attr_accessor :publish_date, :archived
+
   def initialize(publish_date, archived: false)
     @id = Random.rand(0..1000)
     @publish_date = publish_date
     @archived = archived
   end
+
   def can_be_archived?
     Time.now.year - Date.parse(@publish_date).year > 10
   end
+
   def move_to_archive
     return unless can_be_archived?
+
     @archived = true
   end
 end
