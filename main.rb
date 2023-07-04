3c903332ab4f00c12939c27b3d3f5c62c8cf1394
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-04 10:22:29 -1100

    Add main file

---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative 'options'
+
+def main
+  options = Options.new
+  
+  loop do
+    options.display_options
+    option = gets.chomp.to_i
+    break unless options.process_option(option)
+  end
+end
+
+main
