853c41f29611f9717ce42364642d3a3f917d5db6
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-05 02:42:09 +0500

    Implement tests

--- game.rb
+++ game.rb
@@ -5,7 +5,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at, publish_date, archived)
-    super(publish_date, archived: archived)
+    super(publish_date, archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
