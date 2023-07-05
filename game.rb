c4c7a6a9b797e7b0c441d8fe2b2b3984b61049c4
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-05 14:02:56 +0500

    Add test for game and its methods

--- game.rb
+++ game.rb
@@ -5,13 +5,13 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at, publish_date, archived)
-    super(publish_date, archived)
+    super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    years_since_last_played = Time.now.year - Date.parse(last_played_at).year
-    super && years_since_last_played > 2
+    years_since_last_played = Time.now.year - Date.parse(@last_played_at).year > 2
+    super && years_since_last_played
   end
 end
