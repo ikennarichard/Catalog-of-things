c668dfb5150f95d416bfd5581c21c49d88a788b7
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-05 10:52:08 -1100

    Update accessor

--- music_album.rb
+++ music_album.rb
@@ -2,10 +2,10 @@ require_relative 'item'
 require_relative 'genre'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :on_spotify, :title
 
-  def initialize(title, on_spotify: false)
-    super()
+  def initialize(id=nil, title, on_spotify: false)
+    super(id)
     @title = title
     @on_spotify = on_spotify
   end
