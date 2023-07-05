46e58e868380e23b50be44af6832e981173e6fde
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-05 10:58:17 -1100

    Update item properties

--- item.rb
+++ item.rb
@@ -4,14 +4,14 @@ class Item
   attr_reader :id, :archived
   attr_accessor :publish_date, :genre, :author, :source, :label
 
-  def initializer(publish_date, archived: false)
-    @id = Random.rand(0...1000)
-    @publish_date = Date.parse(publish_date)
-    @archived = archived
+  def initialize(publish_date)
+    @id = Random.rand(0..1000)
+    @publish_date = publish_date
+    @archived = false
   end
 
   def can_be_archived?
-    Time.now.year - publish_date.year > 10
+    Time.now.year - Date.parse(publish_date).year > 10
   end
 
   def move_to_archive
