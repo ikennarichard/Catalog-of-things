e23cc22452cf05cfdaf69175ff65831ff1b8459f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-05 10:57:17 -1100

    Update main file

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require_relative 'options'
+require_relative 'app'
 
 def main
   options = Options.new
+  app = App.new
 
   loop do
     options.display_options
     option = gets.chomp.to_i
-    break unless options.process_option(option)
+    break unless options.process_option(app, option)
   end
 end
 
