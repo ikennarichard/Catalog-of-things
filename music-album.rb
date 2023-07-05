7eb6f3eec8561281d97d9f306358e60a5a52e850
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-04 22:39:43 -1100

    Update music album class

--- music-album.rb
+++ music-album.rb
@@ -2,24 +2,15 @@ require_relative 'item'
 require_relative 'genre'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify, :genres, :publish_date
+  attr_accessor :on_spotify
 
-  def initialize(title, publish_date, on_spotify:false)
+  def initialize(title, on_spotify: false)
     super()
-    @on_spotify = on_spotify
     @title = title
-    @genres = []
-    @publish_date = publish_date
+    @on_spotify = on_spotify
   end
 
   def can_be_archived?
     super and on_spotify
-  end
-
-  def add_genre(genre)
-    genre = Genre.new(genre)
-    @genres << genre
-    genre.add_item(self)
-    genre
   end
 end
